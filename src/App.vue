fbfeb0483dbfcae702ef4121431b8690ff6d6847
KJNotfound <kejunwu8@gmail.com>
2025-12-31 15:13:47 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,16 @@ import BottomNav from './components/BottomNav.vue'
 </script>
 
 <template>
-  <div class="min-h-screen bg-white flex flex-col">
-    <router-view />
+  <div class="min-h-[100dvh] bg-white flex flex-col">
+    <router-view class="flex-1 overflow-y-auto" />
+
     <BottomNav />
   </div>
 </template>
 
 <style>
 @import 'tailwindcss';
+
 @theme {
   --color-seen-accent: #635ad9;
 }
